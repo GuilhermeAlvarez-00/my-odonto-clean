a2c9664fdabaa4e8e7d85a84a09d7f5f67213b16
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-01-09 22:48:39 -0300

    fix: adjusting bug in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
     font-family: 'Inter', sans-serif;
   }
 
@@ -23,6 +24,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
+    width: 100%;
     background-color: var(--blue-200);
   }
 
