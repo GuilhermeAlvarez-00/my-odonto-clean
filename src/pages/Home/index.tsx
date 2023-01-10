404edcedebc8c025b33cd90b0fce62cac6fe86be
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-01-09 22:49:14 -0300

    feat: creating home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Menu } from '../../components/Menu';
+
+import { Container } from './styles';
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+};
