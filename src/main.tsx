9f6679b71868dc3b9137acb462a8f8a5311363e5
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-01-18 22:55:54 -0300

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { GlobalStyles } from './styles/global';
+import { DesignSystem } from './pages/DesignSystem';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/design-system',
+    element: <DesignSystem />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
