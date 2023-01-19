47e1939479934e1e053654eb2bfecbba62a78efe
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2023-01-18 22:55:27 -0300

    feat: page for design system

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { FC } from 'react';
 import { Menu } from '../../components/Menu';
+import { Hero } from '../../components/sections/Home/Hero';
 
 import { Container } from './styles';
 
-export const Home: React.FC = () => {
+export const Home: FC = () => {
   return (
     <>
       <Menu />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 };
